5eea76a446d8edf9604af4d91b717f2e524a0c6c
jls83 <josephlsanders@gmail.com>
2022-04-15 16:53:26 +0000

    Break out plugin definitions into lua arrays

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -1,74 +1,102 @@
-return require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
+base_plugins = {
+    'wbthomason/packer.nvim',
 
-    -- I am dead without these
-    use 'tpope/vim-surround'
-    use 'tpope/vim-commentary'
-    use 'tpope/vim-repeat'
-    use {
-        'junegunn/fzf',
-        run = function() vim.fn["fzf#install"]() end,
-        commit = '3f75a83'
-    }
-    use 'junegunn/fzf.vim'
-    use 'gruvbox-community/gruvbox'
-    use 'tpope/vim-vinegar'
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'quangnguyen30192/cmp-nvim-ultisnips'
-    use 'onsails/lspkind-nvim'
+    -- Appearance plugins
+    'gruvbox-community/gruvbox',
+    'itchyny/lightline.vim',
 
-    -- I am mildly inconvenienced without these
-    use 'tpope/vim-fugitive'
-    use 'tpope/vim-rhubarb'
-    use 'easymotion/vim-easymotion'
-    use { 'jls83/vimwiki', branch = 'mixed_backlinks' }
-    -- use { 'vimwiki/vimwiki', branch = 'dev' }
-    use 'itchyny/lightline.vim'
+    -- Editing plugins
+    'tpope/vim-surround',
+    'tpope/vim-commentary',
+    'tpope/vim-repeat',
+    'tpope/vim-vinegar',
 
-    -- Clojure stuff
-    use {
+    -- Vimwiki items
+    { 'jls83/vimwiki', branch = 'mixed_backlinks' },
+    -- { 'vimwiki/vimwiki', branch = 'dev' },
+
+    -- Git plugins
+    'tpope/vim-fugitive',
+    'tpope/vim-rhubarb',
+
+    -- fzf plugins
+    { 'junegunn/fzf', run = function() vim.fn["fzf#install"]() end, commit = '3f75a83' },
+    'junegunn/fzf.vim',
+
+    -- LSP, Completion, and Snippets plugins
+    'neovim/nvim-lspconfig',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'quangnguyen30192/cmp-nvim-ultisnips',
+    'onsails/lspkind-nvim',
+    'honza/vim-snippets',
+    'SirVer/ultisnips',
+
+    -- Telescope items
+    {
+      'nvim-telescope/telescope.nvim',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    },
+    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+    'kyazdani42/nvim-web-devicons',
+
+    -- Other items
+    { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
+    'ojroques/vim-oscyank',
+    'easymotion/vim-easymotion',
+
+}
+
+language_specific_plugins = {
+    -- Clojure items
+    {
         'Olical/conjure',
         tag = 'v4.9.0',
         ft = {'clojure'}
-    }
-    use {
+    },
+    {
         'clojure-vim/vim-jack-in',
         ft = {'clojure'},
         requires = { 'tpope/vim-dispatch', ft = {'clojure'} },
         requires = { 'radenling/vim-dispatch-neovim', ft = {'clojure'} }
-    }
-    use {
+    },
+    {
         'guns/vim-sexp',
         ft = {'clojure'}
-    }
-    use {
+    },
+    {
         'tpope/vim-sexp-mappings-for-regular-people',
         ft = {'clojure'}
-    }
-    use {
+    },
+    {
         'junegunn/rainbow_parentheses.vim',
         ft = {'clojure'}
-    }
+    },
 
     -- Other language-specific items
-    use 'mxw/vim-jsx'
-    use 'cespare/vim-toml'
+    'mxw/vim-jsx',
+    'cespare/vim-toml',
+}
 
-    -- Testing these
-    use 'Yggdroot/indentLine'
-    use 'honza/vim-snippets'
-    use 'SirVer/ultisnips'
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-    use 'ojroques/vim-oscyank'
+testing_plugins = {}
 
-    use {
-      'nvim-telescope/telescope.nvim',
-      requires = { {'nvim-lua/plenary.nvim'} }
+return require('packer').startup(function()
+    plugin_arrays = {
+        base_plugins,
+        language_specific_plugins,
+        testing_plugins,
     }
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-    use 'kyazdani42/nvim-web-devicons'
+
+    if vim.env.MACHINE_TYPE == 'glinux' then
+        require('google.plugins')
+        table.insert(plugin_arrays, google_plugins)
+    end
+
+    for _, plugins in ipairs(plugin_arrays) do
+        for _, v in ipairs(plugins) do
+            use(v)
+        end
+    end
 end)
