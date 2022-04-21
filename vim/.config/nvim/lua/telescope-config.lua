f89042558644f4859e03f794dcdc33a5dcba4fe3
jls83 <josephlsanders@gmail.com>
2022-04-21 16:00:16 +0000

    Change telescope buffers sorting to last used at the bottom

--- vim/.config/nvim/lua/telescope-config.lua
+++ vim/.config/nvim/lua/telescope-config.lua
@@ -1,15 +1,20 @@
-require('telescope').setup()
+require('telescope').setup({
+    pickers = {
+        buffers = {
+            sort_lastused = true,
+        },
+    },
+})
 
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 require('telescope').load_extension('fzf')
 
-
 function my_buffer_window()
     require('telescope.builtin')
         .buffers({
             layout_strategy='bottom_pane',
-            layout_config={prompt_position='bottom'}
+            layout_config={prompt_position='bottom'},
         })
 end
 
