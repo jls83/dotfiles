ed9f9e539e4875fe3bb3ac09a758678df2d8e329
jls83 <josephlsanders@gmail.com>
2022-07-26 14:17:17 +0000

    Move definition binding to use telescope

--- vim/.config/nvim/lua/lsp-config.lua
+++ vim/.config/nvim/lua/lsp-config.lua
@@ -6,7 +6,8 @@ local on_attach = function(_, bufnr)
 
     local opts = { noremap=true, silent=true }
 
-    buf_set_keymap('n', '<leader>gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    -- buf_set_keymap('n', '<leader>gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', '<leader>gd', '<Cmd>lua require("telescope.builtin").lsp_definitions()<CR>', opts)
     buf_set_keymap('n', '<leader>gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
     buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
     buf_set_keymap('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
