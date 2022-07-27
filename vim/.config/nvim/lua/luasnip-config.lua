0858ebc0e10cd4d58efcf1a6afde8e2ab20f26a0
jls83 <josephlsanders@gmail.com>
2022-07-27 12:00:39 -0400

    Add vwdate snippet to luasnip

--- vim/.config/nvim/lua/luasnip-config.lua
+++ vim/.config/nvim/lua/luasnip-config.lua
@@ -1,8 +1,14 @@
 local luasnip = require('luasnip')
+local snippet = luasnip.s
+local t = luasnip.text_node
 
 luasnip.config.set_config({
     history = true,
     updateevents = "TextChanged,TextChangedI"
+})
+
+luasnip.add_snippets("all", {
+    snippet("vwdate", t(os.date("%Y-%m-%d (%A)")));
 })
 
 -- Keymaps stolen from TJ DeVries
@@ -27,3 +33,6 @@ vim.keymap.set("i", "<c-l>", function()
 end)
 
 vim.keymap.set("i", "<c-u>", require "luasnip.extras.select_choice")
+
+-- shorcut to source my luasnips file again, which will reload my snippets
+vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/lua/luasnip-config.lua<CR>")
