09798d36fddf965abefd17014ce0b527af9ba1dc
jls83 <josephlsanders@gmail.com>
2022-12-01 19:40:31 -0500

    Enable incremental selection for treesitter

--- vim/.config/nvim/lua/treesitter-config.lua
+++ vim/.config/nvim/lua/treesitter-config.lua
@@ -18,4 +18,13 @@ require('nvim-treesitter.configs').setup({
     },
     indent = { enable = false },
     highlight = { enable = true },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+        },
+    },
 })
