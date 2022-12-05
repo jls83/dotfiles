85b575c9fde5b169c88c7020281de4a10bd599c5
jls83 <josephlsanders@gmail.com>
2022-12-05 17:06:13 +0000

    Add delete_buffer keybind, clean up other keybinds

--- vim/.config/nvim/lua/telescope-config.lua
+++ vim/.config/nvim/lua/telescope-config.lua
@@ -1,19 +1,26 @@
+local telescope = require('telescope')
+local builtin = require('telescope.builtin')
+
+telescope.setup({
+    pickers = {
+        buffers = {
+            mappings = {
+                i = {['<c-d>'] = 'delete_buffer'},
+            },
+            layout_strategy='bottom_pane',
+            layout_config={prompt_position='bottom'},
+            sort_mru = true,
+            ignore_current_buffer = true,
+        },
+    },
+})
+
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
+telescope.load_extension('fzf')
 
 vim.api.nvim_set_hl(0, "TelescopeSelection", {link = "CursorLine"})
 vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", {link = "TelescopeSelection"})
-
-function my_buffer_window()
-    require('telescope.builtin')
-    .buffers({
-        layout_strategy='bottom_pane',
-        layout_config={prompt_position='bottom'},
-        sort_mru = true,
-        ignore_current_buffer = true,
-    })
-end
 
 function my_lsp_references()
     require('telescope.builtin')
@@ -23,13 +30,5 @@ function my_lsp_references()
     })
 end
 
-
-vim.api.nvim_set_keymap('n', '<leader>b',
-[[<cmd>lua my_buffer_window()<CR>]],
-{ noremap = true, silent=true }
-)
-
-vim.api.nvim_set_keymap('n', '<leader>u',
-[[<cmd>lua my_lsp_references()<CR>]],
-{ noremap = true, silent=true }
-)
+vim.keymap.set('n', '<leader>b', builtin.buffers, {noremap = true, silent = true})
+vim.keymap.set('n', '<leader>u', my_lsp_references, {noremap = true, silent = true})
