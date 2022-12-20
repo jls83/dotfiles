9637e250f00b00ab5d3e3a022aad2a716f48ba39
jls83 <josephlsanders@gmail.com>
2022-12-20 21:17:20 +0000

    Redo keymaps for diagnostic items

--- vim/.config/nvim/lua/diagnostic-config.lua
+++ vim/.config/nvim/lua/diagnostic-config.lua
@@ -9,9 +9,10 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
-local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', '[g', '<cmd>lua vim.diagnostic.goto_prev({})<CR>', opts)
-vim.api.nvim_set_keymap('n', ']g', '<cmd>lua vim.diagnostic.goto_next({})<CR>', opts)
+local opts = { silent = true }
+
+vim.keymap.set('n', '[g', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']g', vim.diagnostic.goto_next, opts)
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
