73f08ec9a8c375cdb7eb81b50ea6a128cd1c75c6
jls83 <josephlsanders@gmail.com>
2022-12-20 21:35:25 +0000

    Add inverted selections for gruvbox

--- vim/.config/nvim/lua/colorscheme-config.lua
+++ vim/.config/nvim/lua/colorscheme-config.lua
@@ -4,6 +4,7 @@ local palette = require('gruvbox.palette')
 gruvbox.setup({
     contrast = '',
     transparent_mode = true,
+    invert_selection = true,
     overrides = {
         SignColumn = { bg = palette.dark1 },
         String = { italic = false },
