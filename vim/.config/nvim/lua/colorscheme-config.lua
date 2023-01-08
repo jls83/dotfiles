823bf849c02d722e9ceb211c5e4d7f142127e0f0
jls83 <josephlsanders@gmail.com>
2023-01-08 18:05:12 +0000

    Change telescope color & borderchars items

--- vim/.config/nvim/lua/colorscheme-config.lua
+++ vim/.config/nvim/lua/colorscheme-config.lua
@@ -1,17 +1,64 @@
 local gruvbox = require('gruvbox')
 local palette = require('gruvbox.palette')
 
+local telescope_borders = {
+  fg = palette.dark2,
+}
+
+local telescope_titles = {
+  fg = palette.faded_aqua,
+  bg = nil,
+  bold = true,
+}
+
+local color_overrides = {
+  -- General overrides
+  SignColumn = { bg = palette.dark1 },
+  String = { italic = false },
+  Operator = { italic = false },
+
+  -- Treesitter items
+  ["@include.rust"] = { link = "Keyword" },
+  ["@type.qualifier"] = { link = "Keyword" },
+  ["@type.builtin"] = { link = "Keyword" },
+
+  -- Telescope items
+  -- Part of the need for explicit overrides is an upstream issue in
+  -- gruvbox.nvim.
+  TelescopePromptBorder = { fg = palette.faded_aqua },
+  TelescopePromptTitle = {
+    fg = palette.dark0_hard,
+    bg = palette.faded_aqua,
+    bold = true,
+  },
+  TelescopePromptNormal = { fg = palette.light1 },
+  TelescopePromptPrefix = { fg = palette.bright_red },
+
+  TelescopePreviewBorder = telescope_borders,
+  TelescopePreviewTitle = telescope_titles;
+
+  TelescopeResultsBorder = telescope_borders,
+  TelescopeResultsTitle = telescope_titles;
+
+  TelescopeSelection = {
+    fg = palette.light1,
+    bg = palette.dark1,
+  },
+
+  TelescopeSelectionCaret = {
+    fg = palette.bright_green,
+    bg = palette.dark1,
+  },
+
+  TelescopeResultsDiffAdd = { fg = palette.bright_green },
+  TelescopeResultsDiffChange = { fg = palette.neutral_yellow },
+  TelescopeResultsDiffDelete = { fg = palette.bright_red },
+}
+
 gruvbox.setup({
-    contrast = '',
-    transparent_mode = true,
-    invert_selection = true,
-    overrides = {
-        SignColumn = { bg = palette.dark1 },
-        String = { italic = false },
-        Operator = { italic = false },
-        ["@include.rust"] = { link = "Keyword" },
-        ["@type.qualifier"] = { link = "Keyword" },
-        ["@type.builtin"] = { link = "Keyword" },
-    },
+  contrast = '',
+  transparent_mode = true,
+  invert_selection = true,
+  overrides = color_overrides,
 })
 vim.cmd([[colorscheme gruvbox]])
--- vim/.config/nvim/lua/telescope-config.lua
+++ vim/.config/nvim/lua/telescope-config.lua
@@ -3,6 +3,12 @@ local builtin = require('telescope.builtin')
 require('entry-display-creator')
 
 telescope.setup({
+    defaults = {
+        -- top, right, bottom, left, NW, NE, SE, SW
+        borderchars = {'═', '│', '═', '│', '╒', '╕', '╛', '╘'},
+        -- These are the true defaults:
+        -- borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    },
     pickers = {
         buffers = {
             mappings = {
@@ -19,9 +25,6 @@ telescope.setup({
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 telescope.load_extension('fzf')
-
-vim.api.nvim_set_hl(0, "TelescopeSelection", {link = "CursorLine"})
-vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", {link = "TelescopeSelection"})
 
 function my_lsp_references()
     require('telescope.builtin')
