66dd26ff4c616a84e984ba4e1818333aa641c3aa
jls83 <josephlsanders@gmail.com>
2023-01-08 19:35:06 -0500

    Remove pyenv shell workaround

--- shell/env_specific_items/all_macbooks.zsh
+++ shell/env_specific_items/all_macbooks.zsh
@@ -1,7 +1,3 @@
-# Workaround for pyenv/bzip2/zlib issue: https://github.com/pyenv/pyenv/issues/1746#issuecomment-738749297
-export LDFLAGS="-L$(brew --prefix zlib)/lib -L$(brew --prefix bzip2)/lib"
-export CPPFLAGS="-I$(brew --prefix zlib)/include -I$(brew --prefix bzip2)/include"
-
 maccopy() {
     cat $1 | pbcopy
 }
