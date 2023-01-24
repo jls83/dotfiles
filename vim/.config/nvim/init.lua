c978eb2fe8e48e75c625f808ad8c41ed39f8e7eb
jls83 <josephlsanders@gmail.com>
2023-01-23 21:17:46 -0500

    Add vimwiki stuff into init.lua

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -2,6 +2,8 @@ require('options')
 require('mappings')
 require('plugins')
 
+local Keymap = require("utils.keymap")
+
 if vim.env.MACHINE_TYPE == 'glinux' then
   vim.cmd('source', '$HOME/.config/nvim/google/my_google.vim')
   require('google.telescope-codesearch-config')
@@ -27,5 +29,27 @@ vim.api.nvim_create_autocmd(
     pattern = "*",
     command = "if &nu | set nornu | endif",
     group = numbertoggle_group,
+  }
+)
+
+-- TODO: Should this live here?
+vim.g.vimwiki_list = {
+  {
+    path = "~/vimwiki",
+    syntax = "markdown",
+    ext = ".md",
+  },
+}
+vim.g.vimwiki_global_ext = 0
+
+-- Add a leading '../' to links to allow for linking to main wiki articles from
+-- the diary.
+Keymap.nnoremap("<leader>wl", "0f[wi../<ESC>")
+
+vim.api.nvim_create_autocmd(
+  {"VimEnter"},
+  {
+    pattern = "*",
+    command = "let g:vimwiki_syntaxlocal_vars['markdown']['Link1'] = g:vimwiki_syntaxlocal_vars['default']['Link1']",
   }
 )
