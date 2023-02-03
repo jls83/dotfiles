92bec110bfea06ce3513428b6b427080ea9b3973
jls83 <josephlsanders@gmail.com>
2023-02-03 18:06:35 +0000

    Add config to turn off 3rd party checks for sumneko

--- vim/.config/nvim/after/plugin/lsp.lua
+++ vim/.config/nvim/after/plugin/lsp.lua
@@ -33,6 +33,13 @@ for _, lsp in ipairs(servers) do
                     -- Get the language server to recognize the `vim` global
                     globals = {'vim'},
                 },
+                workspace = {
+                    library = vim.api.nvim_get_runtime_file('', true),
+                    checkThirdParty = false,
+                },
+                telemetry = {
+                    enable = false,
+                },
             },
         }
     end
