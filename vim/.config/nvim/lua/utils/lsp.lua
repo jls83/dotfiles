29372603316496e542bd40e569c0ca1f7366478d
jls83 <josephlsanders@gmail.com>
2023-03-03 18:53:44 -0500

    Fix up Mason setup junk

--- vim/.config/nvim/lua/utils/lsp.lua
+++ vim/.config/nvim/lua/utils/lsp.lua
@@ -38,6 +38,7 @@ M.capabilities = vim.tbl_deep_extend(
 )
 
 M.mason_setup = function(servers)
+    require('mason').setup()
     require('mason-lspconfig').setup({
         ensure_installed = servers,
     })
