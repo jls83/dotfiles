cb45f4b8de36370bc4a9b91906031bf67c09acce
jls83 <josephlsanders@gmail.com>
2023-03-18 21:11:55 -0400

    Reenable zf keybind

--- vim/.config/nvim/lua/plugin_config/ufo.lua
+++ vim/.config/nvim/lua/plugin_config/ufo.lua
@@ -47,7 +47,7 @@ ufo.setup({
 })
 
 -- TODO: This can cause the folds to freak out or some reason?
--- Keymap.nnoremap("zf", "zcVzCzo")
+Keymap.nnoremap("zf", "zcVzCzo")
 
 -- TODO: Explain
 Keymap.nnoremap("zR", ufo.openAllFolds)
