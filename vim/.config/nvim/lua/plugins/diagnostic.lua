4603da4ca9321c966c3604977a4c75887d0e6ea3
jls83 <josephlsanders@gmail.com>
2024-02-13 18:28:02 +0000

    Remove lsp-lines from diagnostic config

--- vim/.config/nvim/lua/plugins/diagnostic.lua
+++ vim/.config/nvim/lua/plugins/diagnostic.lua
@@ -1,69 +1,43 @@
-return {
-  url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-  dependencies = {
-    "neovim/nvim-lspconfig",
-  },
-  config = function()
-    local lsp_lines = require('lsp_lines')
-    lsp_lines.setup()
+local toggle_float = function()
+  local win_id = vim.g.current_diag_window_id
 
-    vim.diagnostic.config({
-      virtual_text = false,
-      virtual_lines = {
-        only_current_line = true,
-      },
-    })
+  if (win_id ~= nil and vim.api.nvim_win_is_valid(win_id)) then
+    vim.api.nvim_win_close(win_id, true)
+    vim.g.current_diag_window_id = nil
+  else
+    _, win_id = vim.diagnostic.open_float()
+    vim.g.current_diag_window_id = win_id
+  end
+end
 
-    local signs = {
-      Error = " ",
-      Warn = " ",
-      Hint = " ",
-      Info = " "
-    }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  update_in_insert = false,
+})
 
-    local opts = { silent = true }
-
-    local goto_wrap = function(fn)
-      return function()
-        fn({ float = false })
-      end
-    end
-
-    vim.keymap.set('n', '[g', goto_wrap(vim.diagnostic.goto_prev), opts)
-    vim.keymap.set('n', ']g', goto_wrap(vim.diagnostic.goto_next), opts)
-
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-      update_in_insert = false,
-    }
-    )
-
-    -- TODO: Consider toggling `virtual_text` on/off as well.
-    -- Toggles between `only_current_line` and fully off.
-    local lsp_lines_toggle = function()
-      local current_state = vim.diagnostic.config().virtual_lines
-      if (current_state == false) then
-        vim.diagnostic.config({virtual_lines = { only_current_line = true }})
-      else
-        vim.diagnostic.config({virtual_lines = false})
-      end
-    end
-
-    -- Toggles between all diagnostics and fully off.
-    local lsp_lines_full_toggle = function()
-      local current_state = vim.diagnostic.config().virtual_lines
-      if (current_state == true) then
-        vim.diagnostic.config({virtual_lines = { only_current_line = true }})
-      else
-        vim.diagnostic.config({virtual_lines = true})
-      end
-    end
-
-    vim.keymap.set('n', '<leader>l', lsp_lines_toggle, opts)
-    vim.keymap.set('n', '<leader>L', lsp_lines_full_toggle, opts)
-  end,
+local signs = {
+  Error = " ",
+  Warn = " ",
+  Hint = " ",
+  Info = " "
 }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+local opts = { silent = true }
+
+local goto_wrap = function(fn)
+  return function()
+    fn({ float = false })
+  end
+end
+
+vim.keymap.set('n', '[g', goto_wrap(vim.diagnostic.goto_prev), opts)
+vim.keymap.set('n', ']g', goto_wrap(vim.diagnostic.goto_next), opts)
+vim.keymap.set('n', '<leader>l', toggle_float, opts)
+
+-- TODO: move this configuration out of the `plugins` directory.
+return {}
