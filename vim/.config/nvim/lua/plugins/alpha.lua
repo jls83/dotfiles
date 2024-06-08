7601f6a6951e162419eb403249f10d7b8220cf00
jls83 <josephlsanders@gmail.com>
2024-06-08 16:26:34 +0000

    Re-enable alpha.nvim

--- vim/.config/nvim/lua/plugins/alpha.lua
+++ vim/.config/nvim/lua/plugins/alpha.lua
@@ -10,24 +10,25 @@ local header_text = {
 }
 
 return {
-  -- 'goolord/alpha-nvim',
-  -- dependencies = { 'nvim-tree/nvim-web-devicons' },
-  -- config = function()
-  --   local alpha = require('alpha')
-  --   local dashboard = require('alpha.themes.dashboard')
+  'goolord/alpha-nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    local alpha = require('alpha')
+    -- local dashboard = require('alpha.themes.dashboard')
+    local startify = require('alpha.themes.startify')
 
-  --   dashboard.section.buttons.val = {
-  --     dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
-  --     dashboard.button("r", "  Recent"   , ":Telescope oldfiles<CR>"),
-  --     dashboard.button("s", "  Settings" , ":e $MYVIMRC<CR>")
-  --   }
+    -- dashboard.section.buttons.val = {
+    --   dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+    --   dashboard.button("r", "  Recent"   , ":Telescope oldfiles<CR>"),
+    --   dashboard.button("s", "  Settings" , ":e $MYVIMRC<CR>")
+    -- }
 
-  --   dashboard.section.header.val = header_text
+    -- dashboard.section.header.val = header_text
 
-  --   alpha.setup(dashboard.config)
+    alpha.setup(startify.config)
 
-  --   vim.cmd([[
-  --     autocmd FileType alpha setlocal nofoldenable
-  --   ]])
-  -- end
+    vim.cmd([[
+      autocmd FileType alpha setlocal nofoldenable
+    ]])
+  end
 };
