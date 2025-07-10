2e322a3f9e28e646daba6a2997a2f4d3d1652c23
jls83 <josephlsanders@gmail.com>
2025-07-09 20:32:22 -0400

    Move keymaps to plugin file, change call style to use Lua

--- vim/.config/nvim/lua/mappings.lua
+++ vim/.config/nvim/lua/mappings.lua
@@ -8,6 +8,3 @@ Keymap.nnoremap("Y", "y$")
 Keymap.nnoremap("<leader>y", ":%y*<CR>")
 Keymap.vnoremap("<leader>y", '"*y')
 Keymap.nnoremap("<leader>p", '"*p')
-
-Keymap.nnoremap("-", '<CMD>Oil<CR>')
-Keymap.nnoremap("_", ':vsp<CR><CMD>Oil<CR>')
--- vim/.config/nvim/lua/plugins/oil.lua
+++ vim/.config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,22 @@
+local Keymap = require('utils.keymap')
+
 return {
   "stevearc/oil.nvim",
-  enabled = false,
+  enabled = true,
   config = function()
-    require("oil").setup({
+    local oil = require("oil")
+    oil.setup({
       view_options = {
         show_hidden = true,
       },
     })
+
+    Keymap.nnoremap("-", function()
+      oil.open()
+    end)
+    Keymap.nnoremap("_", function()
+      vim.cmd('vsplit')
+      oil.open()
+    end)
   end
 }
