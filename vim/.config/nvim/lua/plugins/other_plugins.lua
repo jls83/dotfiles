914df0fafb0046937c852878068c72cdd5fec317
jls83 <josephlsanders@gmail.com>
2025-08-18 14:29:58 -0400

    Add obsidian.nvim plugin

--- vim/.config/nvim/lua/plugins/other_plugins.lua
+++ vim/.config/nvim/lua/plugins/other_plugins.lua
@@ -81,6 +81,36 @@ return {
     "cshuaimin/ssr.nvim",
     enabled = false,
   },
+  {
+    "obsidian-nvim/obsidian.nvim",
+    enabled = true,
+    version = "*", -- recommended, use latest release instead of latest commit
+    -- ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+      -- refer to `:h file-pattern` for more examples
+      "BufReadPre " .. vim.fn.expand("~") .. "/obsidian_data/*.md",
+      "BufNewFile " .. vim.fn.expand("~") .. "/obsidian_data/*.md",
+    },
+    ---@module 'obsidian'
+    ---@type obsidian.config
+    opts = {
+      workspaces = {
+        {
+          name = "main",
+          path = "~/obsidian_data",
+        },
+      },
+      completion = {
+        nvim_cmp = false,
+        blink = true,
+      },
+
+      -- see below for full list of options 👇
+    },
+  },
   -- Forked plugins
   {
     "jls83/vim-oscyank",
