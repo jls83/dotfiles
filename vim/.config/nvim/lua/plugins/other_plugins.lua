5011f5262fc762aaf3b8e35a957e6f3945195546
jls83 <josephlsanders@gmail.com>
2025-09-18 19:10:16 +0000

    Add glinux-aware enabled logic for obsidian.nvim

--- vim/.config/nvim/lua/plugins/other_plugins.lua
+++ vim/.config/nvim/lua/plugins/other_plugins.lua
@@ -83,7 +83,7 @@ return {
   },
   {
     "obsidian-nvim/obsidian.nvim",
-    enabled = true,
+    enabled = vim.env.MACHINE_TYPE ~= 'glinux',
     version = "*", -- recommended, use latest release instead of latest commit
     ft = "*",
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
